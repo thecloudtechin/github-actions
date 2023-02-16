e7dfcfd6a3c674b6272fd259a22ad2646dd0404f
thecloudtechin <arun@thecloudtech.in>
2023-02-16 20:54:52 +0530

    commit 58

--- infra/main.tf
+++ infra/main.tf
@@ -7,23 +7,23 @@ resource "aws_cloudwatch_event_rule" "console" {
 #data "aws_sns_topic" "existing_data" {
 #  name = "outbox-topic"
 
-}
-
-
-resource "aws_cloudwatch_event_target" "sns" {
-#  for_each  = var.eventbridge_cron_aws_cloudwatch_event_target
-  count = length(data.aws_sns_topic.existing_data)
-  rule      = aws_cloudwatch_event_rule.console.name
-  target_id = data.aws_sns_topic.existing_data[count.index].id
-  arn       = data.aws_sns_topic.existing_data[count.index].id
-  input     = <<JSON
-  {
-    "TenantId":"",
-    "Type":"PKI.Evoya.Shared.Domain.Messages.PurgeOutboxMessage",
-    "CorrelationId":"13ef5142-27fa-4f8e-9934-85a364a5457a"
-  } 
-  JSON
-}
+#}
+#
+#
+#resource "aws_cloudwatch_event_target" "sns" {
+##  for_each  = var.eventbridge_cron_aws_cloudwatch_event_target
+#  count = length(data.aws_sns_topic.existing_data)
+#  rule      = aws_cloudwatch_event_rule.console.name
+#  target_id = data.aws_sns_topic.existing_data[count.index].id
+#  arn       = data.aws_sns_topic.existing_data[count.index].id
+#  input     = <<JSON
+#  {
+#    "TenantId":"",
+#    "Type":"PKI.Evoya.Shared.Domain.Messages.PurgeOutboxMessage",
+#    "CorrelationId":"13ef5142-27fa-4f8e-9934-85a364a5457a"
+#  }
+#  JSON
+#}
 
 # resource "aws_sns_topic" "sns-topic" {
 #   count = length(var.sns_topics)
